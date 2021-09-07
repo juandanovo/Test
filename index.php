55aa2b498536ccf3c19e73f78c77ace23525d273
Juan Novoa <juanda.novo@gmail.com>
2021-09-07 13:19:32 -0500

    first commit

---
+++ index.php
@@ -0,0 +1,44 @@
+
+<!DOCTYPE Html>
+<html>
+<head>
+	<title>Welcome | Good Food</title>
+	<style type="text/css">
+		body{
+			font-family: Tahoma;
+			font-size: 20px;
+			text-align: center;
+		}
+
+	</style>
+
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+  	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+  	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+
+</head>
+<body>
+
+<?php include 'views/partials/navigator.php' ?>
+
+
+	<div class="container-fluid">
+		<div class="row container-center">
+			<div class="col">
+				<h1 class="display-1 text-success"><b>Welcome to Good Food Web site!</b></h1>
+				<br>
+			</div>
+		</div>
+
+		<div class="row text-center">
+			<div class="col">
+				<h2 class="text-dark" style="font-size: 42px"><b>Goodfood<br> = <br>GoodMood</b></h2>
+			</div>
+		</div>
+	</div>
+
+<?php include 'views/partials/footer.php' ?>
+
+</body>
+
+</html>
